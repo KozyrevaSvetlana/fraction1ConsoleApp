171050062b939175712abab33ff3100da7113de0
LanaKo0507 <lanako0507@gmail.com>
2021-03-20 15:27:19 +0300

    сделала простой ввод на консоль и действие в дробями

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,65 @@ namespace fractions
     {
         static void Main(string[] args)
         {
-            Fraction fraction1 = new Fraction(1,11);
-            Fraction fraction2 = new Fraction(8);
-            Fraction fraction3 = fraction1.Divide(fraction2);
-            fraction3.Print();// 
+            Fraction fraction1 = new Fraction(0, 0);
+            Fraction fraction2 = new Fraction(0, 0);
+
+            Console.WriteLine("Ввод первой дроби");
+            Console.WriteLine("Введите числитель");
+            fraction1.Numerator = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите знаменатель. Если хотите ввести целое число, то напишите 1");
+            fraction1.Denomenator = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("дробь успешно создана");
+            Console.WriteLine("");
+            
+            Console.WriteLine("Ввод второй дроби");
+            Console.WriteLine("Введите числитель");
+            fraction2.Numerator = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите знаменатель. Если хотите ввести целое число, то напишите 1");
+            fraction2.Denomenator = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("дробь успешно создана");
+
+            Fraction resultFraction = new Fraction(0, 0);
+            Console.WriteLine("Введите число");
+            Console.WriteLine("1 - Сложение");
+            Console.WriteLine("2 - Вычитание");
+            Console.WriteLine("3 - Умножение");
+            Console.WriteLine("4 - Деление");
+            string answer = Console.ReadLine();
+            int userAnswer = 0;
+            Int32.TryParse(answer, out userAnswer);
+            while (userAnswer<=0|| userAnswer>4)
+            {
+                Console.WriteLine("Введите верное число");
+                Console.WriteLine("1 - Сложение");
+                Console.WriteLine("2 - Вычитание");
+                Console.WriteLine("3 - Умножение");
+                Console.WriteLine("4 - Деление");
+                userAnswer = Convert.ToInt32(Console.ReadLine());
+            }
+            switch (userAnswer)
+            {
+                case 1:
+                    Console.WriteLine("Результат сложения:");
+                    resultFraction = fraction1.Sum(fraction2);//сложение
+                    resultFraction.Print();
+                    break;
+                case 2:
+                    Console.WriteLine("Результат вычитания");
+                    resultFraction = fraction1.Difference(fraction2);//вычитание
+                    resultFraction.Print();
+                    break;
+                case 3:
+                    Console.WriteLine("Результат умножения");
+                    resultFraction = fraction1.Multiply(fraction2);//умножение
+                    resultFraction.Print();
+                    break;
+                case 4:
+                    Console.WriteLine("Результат деления");
+                    resultFraction = fraction1.Divide(fraction2);//деление
+                    resultFraction.Print();
+                    break;
+            }
         }
     }
 }
